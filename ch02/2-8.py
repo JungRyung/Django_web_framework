d29db25ab5f81b718e0d5096754e1ef06ed240e3
JungRyung <37282346+JungRyung@users.noreply.github.com>
2020-07-25 01:38:03 +0900

    update django

---
+++ ch02/2-8.py
@@ -0,0 +1,20 @@
+from http.client import HTTPConnection
+
+host = 'www.example.com'
+conn = HTTPConnection(host)
+conn.request('GET', '/')
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+
+data1 = r1.read()
+# 일부만 읽는 경우는, 두 번째 요청 시 에러 남
+# data1 = r1.read(100)
+
+# 두 번쨰 요청에 대한 테스트
+conn.request('GET', '/')
+r2 = conn.getresponse()
+print(r2.status, r2.reason)
+
+data2 = r2.read()
+print(data2.decode())
+conn.close()
---
+++ ch02/2-9.py
@@ -0,0 +1,8 @@
+from http.client import HTTPConnection
+conn = HTTPConnection('www.example.com')
+conn.request('HEAD', '/')
+resp = conn.getresponse()
+print(resp.status, resp.reason)
+data = resp.read()
+print(len(data))
+print(data == b'')
